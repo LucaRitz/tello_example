032b6fdbd0fc3ca0959da95a04ad6e93fabce1e4
LucaRitz <ritz.luca@gmail.com>
2020-04-29 18:14:34 +0200

    Added opencv

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include <tello/tello.hpp>
 #include <tello/command.hpp>
 #include <tello/response.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
 
 #define TELLO_IP_ADDRESS (ip_address)0xC0A80A01 // 192.168.10.1
 
@@ -19,6 +22,10 @@ using tello::Status;
 using std::string;
 using ComPtr = std::optional<std::unique_ptr<Command>>;
 
+using std::cout;
+using std::endl;
+using namespace cv;
+
 int main() {
     LoggerSettings settings {"./log/command_log.log", "./log/video_log.log", "./log/status_log.log"};
     Logger::initialize(settings);
@@ -32,6 +39,21 @@ int main() {
     std::unique_ptr<Response> responseCommand = tello.exec(*(command.value()));
 
     Network::disconnect();
+
+    // Opencv
+    Mat image;
+    image = imread("./resources/test.jpg");   // Read the file
+
+    if(! image.data )                              // Check for invalid input
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+
+    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+    imshow( "Display window", image );                   // Show our image inside it.
+
+    waitKey(0);
 
     return 0;
 }
